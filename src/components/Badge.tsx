13b55dc1552cc307c02ede9d42b84e426d1406e1
AmaanUsmani <amaanusmani@gmail.com>
2024-09-19 22:02:12 +0400

    update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -10,31 +10,6 @@ const defaultTextColor = '#303235';
 
 export const Badge = (props: Props) => {
   let liteBadge: HTMLAnchorElement | undefined;
-  let observer: MutationObserver | undefined;
-
-  const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
-    mutations.forEach((mutation) => {
-      mutation.removedNodes.forEach((removedNode) => {
-        if ('id' in removedNode && liteBadge && removedNode.id == 'lite-badge') {
-          console.log("Sorry, you can't remove the brand 😅");
-          props.botContainer?.append(liteBadge);
-        }
-      });
-    });
-  };
-
-  onMount(() => {
-    if (!document || !props.botContainer) return;
-    observer = new MutationObserver(appendBadgeIfNecessary);
-    observer.observe(props.botContainer, {
-      subtree: false,
-      childList: true,
-    });
-  });
-
-  onCleanup(() => {
-    if (observer) observer.disconnect();
-  });
 
   return (
     <span
@@ -47,7 +22,7 @@ export const Badge = (props: Props) => {
       Built by
       <a
         ref={liteBadge}
-        href={'https://raen.ai'}
+        href={'https://google.ae'} // Change this to your desired URL
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
