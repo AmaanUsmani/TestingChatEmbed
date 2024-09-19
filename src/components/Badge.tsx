3e1e059df3bb2815775b5ddc459c3a67e9184751
AmaanUsmani <amaanusmani@gmail.com>
2024-09-19 21:55:30 +0400

    update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Raen AI</span>
+        <span> Amaan Usmani</span>
       </a>
     </span>
   );
